9fa12231a9fef50ece650c8ef01f6a58009feee4
LaNice Powell <lanicemp@aol.com>
2019-07-25 18:31:48 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,9 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        :Benvolio =>{},
+          {:name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"},
         :Mercutio => {}
         #your hashes here!
       ]
