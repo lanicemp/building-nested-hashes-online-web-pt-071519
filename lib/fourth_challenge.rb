551eb301c715bae06b6445f79751fbc0b2289c68
LaNice Powell <lanicemp@aol.com>
2019-07-25 18:37:38 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -8,7 +8,7 @@ def fourth_challenge
           {:name => "Benvolio",
           :age => "17",
           :attitude => "worried"},
-          {:name => "Mercuito",
+          {:name => "Mercutio",
           :age => "18",
           :attitude => "hot-headed"}
         #your hashes here!
